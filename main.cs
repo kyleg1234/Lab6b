006dd0b10845e2f34cdba9c42f40e494c6042246
kyleg123 <66087622+kyleg1234@users.noreply.github.com>
2021-07-02 23:18:56 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+class MainClass {
+  public static void Main (string[] args) {
+
+Console.WriteLine("Enter 10 numbers");
+int counter =0;
+
+while(counter < 10)
+{
+  int num = Convert.ToInt32(Console.ReadLine());
+  counter++;
+int remainder = num %2;
+}
+if (remainder ==0)
+  {
+int[] EvenNum = new int[10];
+  }
+else 
+
+{int[] OddNum = new int[10];
+
+Console.WriteLine(EvenNum);
+Console.WriteLine(OddNum);
+
+}
+Array.Sort(EvenNum);
+Array.Reverse(OddNum);
+int sum = EvenNum.Sum();
+int sum = OddNum.Sum();
+
+
+}
+
+}
+
+
+
